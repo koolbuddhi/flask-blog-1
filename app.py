fe4ff9befe4327b8b04bfb7b4e91773510cc47a7
Buddhima Wickramasinghe <buddhi@bistecglobal.com>
2022-02-14 11:38:23 +0530

    basic db wireup

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
+import sqlite3
+from werkzeug.exceptions import abort
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+def get_db_connection():
+    conn = sqlite3.connect('database.sqlite')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
